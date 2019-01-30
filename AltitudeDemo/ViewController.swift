4a126042590e4beddf4f991d77bda257f3e80f8d
Elvis Nuñez <elvisnunez@me.com>
2019-01-30 13:05:24 +0100

    Update demo

--- AltitudeDemo/ViewController.swift
+++ AltitudeDemo/ViewController.swift
@@ -1,20 +1,62 @@
-//
-//  ViewController.swift
-//  AltitudeDemo
-//
-//  Created by Elvis Nuñez on 30/01/2019.
-//  Copyright © 2019 Elvis Nuñez. All rights reserved.
-//
-
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+    lazy var altimeter: CMAltimeter = {
+        CMAltimeter()
+    }()
+
+    lazy var floorLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 22)
+        label.textAlignment = .center
+        label.textColor = .white
+        return label
+    }()
+
+    lazy var altitudeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.textAlignment = .center
+        label.textColor = .white
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        view.backgroundColor = .black
+        view.addSubview(floorLabel)
+        view.addSubview(altitudeLabel)
+        NSLayoutConstraint.activate([
+            floorLabel.topAnchor.constraint(equalTo: view.topAnchor),
+            floorLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            floorLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+
+            altitudeLabel.topAnchor.constraint(equalTo: floorLabel.bottomAnchor),
+            altitudeLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
+            altitudeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            altitudeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            ])
+
+        if CMAltimeter.isRelativeAltitudeAvailable() {
+            altimeter.startRelativeAltitudeUpdates(to: OperationQueue.main, withHandler: { data, error in
+                if let error = error {
+                    self.floorLabel.text = error.localizedDescription
+                } else if let data = data {
+                    if data.relativeAltitude.floatValue < 0 {
+                        self.floorLabel.text = "Floor 1"
+                    } else {
+                        self.floorLabel.text = "Floor \(data.relativeAltitude.floatValue / 2)"
+                    }
+                    self.altitudeLabel.text = "Altitude \(data.relativeAltitude.stringValue)"
+                } else {
+                    self.floorLabel.text = "No results"
+                }
+            })
+        }
     }
-
-
 }
 
